853a470951daab9a513c01c17d90e66d250d186c
ccoenraets <ccoenraets@gmail.com>
2014-05-28 15:50:13 -0400

    initial commit

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ app.get('/', function (req, res) {
     res.render('index.jade', {name: 'Christophe Coenraets'});
 });
 
+app.post('/signedrequest', function(req, res) {
+    res.render('index.jade', {name: 'Christophe Coenraets'});
+});
+
+//function processSignedRequest(req, res) {
+//    var shipment = new Shipment();
+//    try {
+//        var json = shipment.processSignedRequest(req.body.signed_request, config.APP_SECRET);
+//        res.render("index", json);
+//    } catch (e) {
+//        res.render("error", {
+//            "error": errors.SIGNED_REQUEST_PARSING_ERROR
+//        });
+//    }
+//}
+
 app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function () {
