3fb11ed8da1890681f7bac27560f267fb18dc194
ccoenraets <ccoenraets@gmail.com>
2014-05-29 09:22:03 -0400

    initial commit

--- server.js
+++ server.js
@@ -5,57 +5,30 @@ var express = require('express'),
 
 var app = express();
 
-app.use(bodyParser());          // pull information from html in POST
-
-app.get('/', function (req, res) {
-    res.render('index.jade', {name: 'Christophe Coenraets'});
-});
+app.use(bodyParser()); // pull information from html in POST
 
 app.post('/signedrequest', function(req, res) {
 
     var signedRequest = req.body.signed_request,
-        appSecret = process.env.APP_SECRET;
+        appSecret = process.env.APP_SECRET,
+        sfContext = decode(signedRequest, appSecret),
+        oauthToken = sfContext.client.oauthToken,
+        instanceUrl = sfContext.client.instanceUrl,
 
-    var sfContext = decode(signedRequest, appSecret);
+        query = "SELECT Id, Name FROM Contact LIMIT 10",
 
-    var oauthToken = sfContext.client.oauthToken;
-    var instanceUrl = sfContext.client.instanceUrl;
+        contactRequest = {
+            url: instanceUrl + '/services/data/v29.0/query?q=' + query,
+            headers: {
+                'Authorization': 'OAuth ' + oauthToken
+            }
+        };
 
-    console.log(oauthToken);
-    console.log(instanceUrl);
-
-
-    var q = "SELECT Id, Name FROM Contact LIMIT 10";
-    var r = {
-        url: instanceUrl + '/services/data/v29.0/query?q=' + q,
-        headers: {
-            'Authorization': 'OAuth ' + oauthToken
-        }
-    };
-
-    console.log(JSON.stringify(r));
-
-    request(r, function(err, response, body) {
-        console.log('data');
-        console.log('err: ' + err);
-        console.log('response: ' + response);
-        console.log('body: ' + body);
-        res.render('index.jade', {contacts: JSON.parse(body).records});
+    request(contactRequest, function(err, response, body) {
+        res.render('index.jade', {user: sfContext.user.fullName, contacts: JSON.parse(body).records});
     });
 
 });
-
-//function processSignedRequest(req, res) {
-//    var shipment = new Shipment();
-//    try {
-//        var json = shipment.processSignedRequest(req.body.signed_request, config.APP_SECRET);
-//        res.render("index", json);
-//    } catch (e) {
-//        res.render("error", {
-//            "error": errors.SIGNED_REQUEST_PARSING_ERROR
-//        });
-//    }
-//}
 
 app.set('port', process.env.PORT || 5000);
 
