c7944ec326c1f5d42b8548bb41dfe57efd2d5121
Kunal garg <77530977+Kunalgarg925@users.noreply.github.com>
2023-10-17 11:16:09 +0530

    print log consumer secret key

--- server.js
+++ server.js
@@ -45,4 +45,5 @@ app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
+    console.log('Consumer Secret Key ----> ', consumerSecret);
 });
